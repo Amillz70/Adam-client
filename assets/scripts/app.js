cd10492a7ff6a23ee0dc06e8aa97b42a2ad92977
Amillz70 <adammiller7@yahoo.com>
2018-09-26 15:16:57 -0400

    Made Game Board

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -10,5 +10,5 @@ $(() => {
   $('#sign-up-form').on('submit', authEvents.onSignUp)
   $('#sign-in-form').on('submit', authEvents.onSignIn)
   $('#change-password-form').on('submit', authEvents.onChangePassword)
-  $('#sign-out-button').on('click', authEvents.onSignOut)
+  $('#sign-out-button').on('click', authEvents.onSignOut) // .hide()
 })
