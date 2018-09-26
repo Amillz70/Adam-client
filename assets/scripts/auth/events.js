a04826e5bb152ca3b69c1a076aedf623ec198de0
Amillz70 <adammiller7@yahoo.com>
2018-09-26 10:53:26 -0400

    Added new files for game's API

---
+++ assets/scripts/auth/events.js
@@ -0,0 +1,15 @@
+'use strict'
+const getFormFields = require('../../../lib/get-form-fields.js'
+
+const onSignUp = function (event) {
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  console.log(data)
+  api.signUp(data)
+    .then(console.log)
+    .catch(console.log)
+}
+
+module.exports = {
+onSignUp
+}
