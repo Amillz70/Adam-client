c630968da14d36c7a96bbd04dcf6efed62426d91
Amillz70 <adammiller7@yahoo.com>
2018-09-28 11:58:27 -0400

    Update shell working

--- curl-scripts/update.sh
+++ curl-scripts/update.sh
@@ -1,15 +1,18 @@
-curl "https://tic-tac-toe-wdi.herokuapp.com/" + `/games/:id`\
+
+curl "https://tic-tac-toe-wdi.herokuapp.com/games/${ID}" \
 --include \
   --request PATCH \
-  --header "Authorization: Token token=${TOKEN}"\
-  --data \
-{
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
+  --data '{
   "game": {
-        "id" : 1
+        "id" : 836,
     "cell": {
       "index": 0,
       "value": "x"
     },
     "over": false
   }
-}
+}'
+
+echo
