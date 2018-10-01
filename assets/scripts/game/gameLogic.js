04f8f479459ef943790bf5d9255297f03b9a3af5
Amillz70 <adammiller7@yahoo.com>
2018-10-01 09:58:58 -0400

    Merge wasn't working right, pasted to master

--- assets/scripts/game/gameLogic.js
+++ assets/scripts/game/gameLogic.js
@@ -1,57 +1,114 @@
 // Have constant variables needed for game
 // Need: users, turn, and amount of turns for each(1)
 // let game ={
-const user1 = 'X'
-const user2 = 'O'
-currentUser : ''
-const moves = 1
+// let user1 = 'x'
+// const user2 = 'O'
+
+const board = ['', '', '', '', '', '', '', '', '']
 // }
 
-const cells = querySelctorAll('.tic-box')
+// const gameStart
+// I don't remeber what this was, look into it
+// const cells = querySelctorAll('.tic-box')
 
 // X goes first
 //
+// clickBox
 
-// Win conditions
-const win = new Array
+let user1 = 'x'
+
+const playerSwitch = function() {
+if (user1 === 'x') {
+  user1 ='o'
+} else {
+  user1 ='x'
+}
+}
+
+// const gameWin = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+// // Win conditions
+// function checkWin(user1)
+// {
+//   return gameWin.some(function()) {
+//     return threeInARow.every(function(square)
+//       return board[square] === user1
+//   )
+//   }
+// }
 // Win for user1 or 'X'
-if ($('#squareZero').hasClass('x') && $('#squareOne').hasClass('x') && $('#squareTwo').hasClass('x') ||
-($('#squareThree').hasClass('x') && $('#squareFour').hasClass('x') && $('#squareFive').hasClass('x') ||
-($('#squareSix').hasClass('x') && $('#squareSeven').hasClass('x') && $('#squareEight').hasClass('x') ||
-($('#squareZero').hasClass('x') && $('#squareThree').hasClass('x') && $('#squareSix').hasClass('x') ||
-($('#squareOne').hasClass('x') && $('#squareFour').hasClass('x') && $('#squareSeven').hasClass('x') ||
-($('#squareTwo').hasClass('x') && $('#squareFive').hasClass('x') && $('#squareEight').hasClass('x') ||
-($('#squareZero').hasClass('x') && $('#squareFour').hasClass('x') && $('#squareEight').hasClass('x') ||
-($('#squareTwo').hasClass('x') && $('#squareFour').hasClass('x') && $('#squareSix').hasClass('x')
-)
-{
-  // User1 wins
-}
-// Win for user2 or 'O'
-else if ($('#squareZero').hasClass('o') && $('#squareOne').hasClass('o') && $('#squareTwo').hasClass('o') ||
-($('#squareThree').hasClass('o') && $('#squareFour').hasClass('o') && $('#squareFive').hasClass('o') ||
-($('#squareSix').hasClass('o') && $('#squareSeven').hasClass('o') && $('#squareEight').hasClass('o') ||
-($('#squareZero').hasClass('o') && $('#squareThree').hasClass('o') && $('#squareSix').hasClass('o') ||
-($('#squareOne').hasClass('o') && $('#squareFour').hasClass('o') && $('#squareSeven').hasClass('o') ||
-($('#squareTwo').hasClass('o') && $('#squareFive').hasClass('o') && $('#squareEight').hasClass('o') ||
-($('#squareZero').hasClass('o') && $('#squareFour').hasClass('o') && $('#squareEight').hasClass('o') ||
-($('#squareTwo').hasClass('o') && $('#squareFour').hasClass('o') && $('#squareSix').hasClass('o')
-)
-{
-  // User2 wins
-}
-// For draw
-else if (count === 9){
-  //draw
+const gameWin = function() {
+  {
+if (board[0] !== '' && board[0] === board[1] === board[2]) {
+  return ('Winner is' + board[1])
+} else if (board[3] !== '' && board[3] === board[4] === board[5]) {
+    return ('Winner is' + board[3])
+  } else if (board[6] !== '' && board[6] === board[7] === board[8]) {
+    return ('Winner is' + board[6])
+  } else if (board[0] !== '' && board[0] === board[4] === board[8]) {
+    return ('Winner is' + board[0])
+} else if (board[2] !== '' && board[2] === board[4] === board[6]) {
+  return ('Winner is' + board[2])
+} else if (board[0] !== '' && board[0] === board[3] === board[6]) {
+    return ('Winner is' + board[0])
+} else if (board[1] !== '' && board[1] === board[4] === board[7]) {
+      return ('Winner is' + board[1])
+} else if (board[2] !== '' && board[2] === board[5] === board[8]) {
+        return ('Winner is' + board[2]) }
+else{
+return 'Continue Playing'
 }
 
 
 
 
-function winCondition( ,){
-for (let i = 0; i < winLines[lastMove].length; i++)
-}
+
+// for (let i = 0; i < winLines[lastMove].length; i++){
+// if ($('#squareZero').hasClass('x') && $('#squareOne').hasClass('x') && $('#squareTwo').hasClass('x')) ||
+// ($('#squareThree').hasClass('x') && $('#squareFour').hasClass('x') && $('#squareFive').hasClass('x')) ||
+// ($('#squareSix').hasClass('x') && $('#squareSeven').hasClass('x') && $('#squareEight').hasClass('x')) ||
+// ($('#squareZero').hasClass('x') && $('#squareThree').hasClass('x') && $('#squareSix').hasClass('x')) ||
+// ($('#squareOne').hasClass('x') && $('#squareFour').hasClass('x') && $('#squareSeven').hasClass('x')) ||
+// ($('#squareTwo').hasClass('x') && $('#squareFive').hasClass('x') && $('#squareEight').hasClass('x')) ||
+// ($('#squareZero').hasClass('x') && $('#squareFour').hasClass('x') && $('#squareEight').hasClass('x')) ||
+// ($('#squareTwo').hasClass('x') && $('#squareFour').hasClass('x') && $('#squareSix').hasClass('x'))}
+// for ()
+// {
+// return 'Winner is' + 'X'
+//   // User1 wins
+// }
+// // Win for user2 or 'O'
+// else if ($('#squareZero').hasClass('o') && $('#squareOne').hasClass('o') && $('#squareTwo').hasClass('o')) ||
+// ($('#squareThree').hasClass('o') && $('#squareFour').hasClass('o') && $('#squareFive').hasClass('o')) ||
+// ($('#squareSix').hasClass('o') && $('#squareSeven').hasClass('o') && $('#squareEight').hasClass('o')) ||
+// ($('#squareZero').hasClass('o') && $('#squareThree').hasClass('o') && $('#squareSix').hasClass('o')) ||
+// ($('#squareOne').hasClass('o') && $('#squareFour').hasClass('o') && $('#squareSeven').hasClass('o')) ||
+// ($('#squareTwo').hasClass('o') && $('#squareFive').hasClass('o') && $('#squareEight').hasClass('o')) ||
+// ($('#squareZero').hasClass('o') && $('#squareFour').hasClass('o') && $('#squareEight').hasClass('o')) ||
+// ($('#squareTwo').hasClass('o') && $('#squareFour').hasClass('o') && $('#squareSix').hasClass('o'))
+// )
+// {
+//   // User2 wins
+//   return 'Winner is' + 'O'
+// }
+// if else {
+//
+// }
+// // For draw
+// else  {
+//   //draw
+//   return 'Draw, try again.'
+// }
+// }
+//
+//
+//
+//
+// function winCondition( ,){
+// for (let i = 0; i < winLines[lastMove].length; i++)
+// }
 
 // Says player has won, lost, or drew
 
-// Resets game
+module.exports = {
+  user1
+}
